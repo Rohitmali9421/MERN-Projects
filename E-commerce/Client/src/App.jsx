214eb6940fc4284dfd97eda5ba3d602f3a66d6a6
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-01 18:15:41 +0530

    started frontend

--- E-commerce/Client/src/App.jsx
+++ E-commerce/Client/src/App.jsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
 
-      </BrowserRouter>
     </>
   )
 }
--- E-commerce/Client/src/main.jsx
+++ E-commerce/Client/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Routes } from 'react-router-dom'
+import Login from './Components/Login/Login.jsx'
+import SignUp from './Components/Signup/SignUp.jsx'
+import Layout from './Components/Layout/Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import ProductDetails from './Components/Product/ProductDetails.jsx'
+import Product from './Components/Product/Product.jsx'
+import Cart from './Components/Cart/Cart.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path='/' element={<Layout />} >
+        <Route path='' element={<Home />} />
+        <Route path="product/:id" element={<ProductDetails />} />
+        <Route path="product" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
+    </>
+
+
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
