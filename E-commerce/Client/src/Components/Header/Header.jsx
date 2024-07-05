9c47e690cbb0d2f3a754320b9cdee9be4dbea190
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-05 21:33:53 +0530

    started creating navbar

--- E-commerce/Client/src/Components/Header/Header.jsx
+++ E-commerce/Client/src/Components/Header/Header.jsx
@@ -1,46 +1,66 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-
+import { FaBars } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
+import { IoMdSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { LiaCreativeCommonsSampling } from "react-icons/lia";
 function Header() {
   return (
-    <header className='w-full  h-16 flex items-center justify-between border '>
-      <nav className='px-8 flex items-center justify-between w-full ' >
-        <div className="">
-          <Link to="/">
-            Rohit
+    <header className='w-full  h-16 flex items-center border justify-center'>
+      <nav className='px-4 flex items-center justify-between w-full max-w-[1300px]' >
+
+        <div className="flex items-center ">
+          <FaBars className='mr-4 text-3xl md:hidden' />
+          <Link to="/" className=' text-blue-700 text-2xl font-bold flex items-center'>
+            <LiaCreativeCommonsSampling className=' mr-1' />
+            PeakMart .
           </Link>
         </div>
-        <div >
+
+        <div className='hidden md:flex font-serif' >
           <NavLink
-            to="/"
+            to="/login"
             className={({ isActive }) =>
-              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4 font-bold `
+              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4 font-medium `
             }>
             HOME
           </NavLink>
           <NavLink
-            to="/"
+            to="/login"
             className={({ isActive }) =>
-              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4 font-bold `
+              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4  `
             }>
             ABOUT
           </NavLink>
           <NavLink
-            to="/"
+            to="/login"
             className={({ isActive }) =>
-              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4 font-bold `
+              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4  `
             }>
             PRODUCTS
           </NavLink>
           <NavLink
-            to="/"
+            to="/login"
             className={({ isActive }) =>
-              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4 font-bold `
+              ` ${isActive ? "text-orange-700" : "text-gray-700"} mx-4  `
             }>
             SUPPORTS
           </NavLink>
         </div>
-        <div>
+
+        <div className='flex text-xl'>
+          <div className='flex bg-blue-100  rounded-lg '>
+            <input type="text" className='bg-transparent' />
+            <IoMdSearch className='mx-2 text-xl w-full h-full' />
+          </div>
+
+          <Link className='mx-4 '>
+            <FaCartPlus className='text-xl w-full h-full' />
+          </Link>
+          <div className='py-2'>
+            <FaRegUser />
+          </div>
 
         </div>
       </nav>
--- E-commerce/Client/src/Components/Home/Home.jsx
+++ E-commerce/Client/src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@
 
 function Home() {
  return(
-  <h1> Home</h1>
+  <h1 className="h-screen w-full bg-blue-100"> Home</h1>
  )
 }
 
