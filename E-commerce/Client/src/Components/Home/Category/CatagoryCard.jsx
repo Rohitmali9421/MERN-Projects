eb619d16a3b747f4a1165aca4454efb0b9137ddc
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-07 19:20:12 +0530

    category scroll done

--- E-commerce/Client/src/Components/Home/Category/CatagoryCard.jsx
+++ E-commerce/Client/src/Components/Home/Category/CatagoryCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CatagoryCard({title,imageUrl}) {
     return (
-        <div className="border w-36  h-36 shadow-lg rounded-md hover:bg-blue-100">
+        <div className=" inline-block border w-24 md:w-36 mx-1 md:mx-4 h-24 md:h-36 shadow-lg rounded-md hover:bg-blue-100">
             <img className="mx-auto w-[50%] my-[10%]" src="./catagory.png" alt="" />
             <h1 className="text-center font-semibold ">Phones</h1>
         </div>
--- E-commerce/Client/src/Components/Home/Category/Category.jsx
+++ E-commerce/Client/src/Components/Home/Category/Category.jsx
@@ -3,9 +3,14 @@ import CatagoryCard from './CatagoryCard'
 
 function Category() {
     return (
-        <div className="bg-white pt-4 pb-10">
+        <div className="bg-white pt-4 pb-8 px-2 md:px-10 max-w-[1300px] mx-auto">
             <h1 className="text-blue-500 text-center font-bold text-3xl my-5">Catagories</h1>
-            <div className="flex gap-3 mx-10 justify-center overflow-hidden">
+            <div id='slider' className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+                <CatagoryCard />
+                <CatagoryCard />
+                <CatagoryCard />
+                <CatagoryCard />
+                <CatagoryCard />
                 <CatagoryCard />
                 <CatagoryCard />
                 <CatagoryCard />
