8b755082a578b1c691a9126bbdba27ee859423eb
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-09 21:15:55 +0530

    .minor

--- E-commerce/Client/src/Components/Footer/Footer.jsx
+++ E-commerce/Client/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
 
 
-    <footer class=" shadow bg-gray-900 mt-6">
+    <footer class=" shadow bg-gray-800 mt-6">
       <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
           <a href="" class="flex items-center justify-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
