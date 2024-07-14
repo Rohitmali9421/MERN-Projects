6e3248784fd4cef9d4743b47350c70c4c426c41e
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-14 22:49:25 +0530

    dashboard

--- E-commerce/Client/src/Components/AdminDashboard/AdminDashboard.jsx
+++ E-commerce/Client/src/Components/AdminDashboard/AdminDashboard.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import { FaBars } from "react-icons/fa";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
-
+import { MdDashboard } from "react-icons/md";
+import { FaBoxOpen } from "react-icons/fa";
+import { IoMdCart } from "react-icons/io";
+import { LiaCreativeCommonsSampling } from "react-icons/lia";
+import { GiNotebook } from "react-icons/gi";
+import { BiSolidCoupon } from "react-icons/bi";
+import { BiLogOut } from "react-icons/bi";
+import { Link,  Outlet } from 'react-router-dom'
 function AdminDashboard() {
     const [menu, toggleMenu] = useState(false);
 
@@ -11,18 +18,65 @@ function AdminDashboard() {
 
     return (
         <>
+
             <div className={`${menu ? "" : "-translate-x-full"} fixed top-0 left-0 z-40 w-64 h-screen transition-transform md:translate-x-0 bg-white`}>
-                <div className='flex justify-between m-6 items-center'>
-                    <h1 className='font-bold text-2xl text-blue-500'>PeekMart</h1>
-                    <IoIosArrowDropleftCircle className='md:hidden block' onClick={handleMenuToggle} />
+                <div className='px-4 flex justify-between  items-center h-20 border-b'>
+                    <div className='flex items-center '>
+                        <LiaCreativeCommonsSampling className='mr-2 text-3xl text-blue-500' />
+                        <h1 className='font-bold text-2xl  text-blue-500'>PeekMart</h1>
+                    </div>
+                    <IoIosArrowDropleftCircle className='md:hidden block text-blue-500 text-xl' onClick={handleMenuToggle} />
+                </div>
+                <div className=' flex flex-col justify-between px-4 h-full'>
+                    <ul>
+                        <Link to="/admin">
+                            <li className='flex items-center my-3 bg-transparent  hover:bg-blue-50 rounded-md px-4 py-2'>
+                                <MdDashboard className='text-xl text-blue-500 mr-3' />
+                                <h1 className='font-bold'>Dashboard</h1>
+                            </li>
+                        </Link>
+                        <Link to="/admin/products" >
+                            <li className='flex items-center my-3 hover:bg-blue-50 rounded-md px-4 py-2'>
+                                <FaBoxOpen className='text-xl text-blue-500 mr-3' />
+                                <h1 className='font-bold'>Products</h1>
+                            </li>
+                        </Link >
+                        <Link to="/admin/category">
+                            <li className='flex items-center my-3 hover:bg-blue-50 rounded-md px-4 py-2'>
+                                <IoMdCart className='text-xl text-blue-500 mr-3' />
+                                <h1 className='font-bold'>Category</h1>
+                            </li>
+                        </Link >
+                        <Link to="/admin/orders" >
+                            <li className='flex items-center my-3 hover:bg-blue-50 rounded-md px-4 py-2'>
+                                <GiNotebook className='text-xl text-blue-500 mr-3' />
+                                <h1 className='font-bold'>Orders</h1>
+                            </li>
+                        </Link >
+                        <Link to="/admin/coupons" >
+                            <li className='flex items-center my-3 hover:bg-blue-50 rounded-md px-4 py-2'>
+                                <BiSolidCoupon className='text-xl text-blue-500 mr-3' />
+                                <h1 className='font-bold'>Coupons</h1>
+                            </li>
+                        </Link >
+                    </ul>
+                    <ul className='mb-32 '>
+                        <li className='flex items-center my-3 hover:bg-blue-50 rounded-md px-4 py-2'>
+                            <BiLogOut className='text-xl text-red-600 mr-3' />
+                            <h1 className='font-bold'>Logout</h1>
+                        </li>
+
+                    </ul>
+
                 </div>
 
             </div>
 
-            <div className="w-full h-screen bg-blue-100 md:pl-64">
-                <nav>
+            <div className="w-full h-screen bg-blue-50 md:pl-64">
+                <nav className='w-full h-20 bg-white border'>
                     <FaBars className='md:hidden block ' onClick={handleMenuToggle} />
                 </nav>
+                <Outlet />
             </div>
         </>
     );
