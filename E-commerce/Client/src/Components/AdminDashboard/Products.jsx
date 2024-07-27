22f9769e7659d094628897b262a91339b614b94d
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-27 18:55:40 +0530

    .

--- E-commerce/Client/src/Components/AdminDashboard/Products.jsx
+++ E-commerce/Client/src/Components/AdminDashboard/Products.jsx
@@ -5,24 +5,57 @@ import { IoSearch } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
 import axios from 'axios';
 import { useCategory } from '../../Contexts/CategoryContext';
+import { TiWarning } from "react-icons/ti";
+
 function Products() {
+  const [loder, setloder] = useState(false)
   const [Product, setProduct] = useState(null);
   const category = useCategory()
-
+  const [popup, setPopup] = useState(false);
+  const [deleteProductId, setDeleteProductId] = useState(null);
+  const deleteProduct = async () => {
+    //Implement it after time
+  };
   const fetchProducts = async () => {
     try {
+      setloder(true)
       const response = await axios.get('http://localhost:8000/api/products');
       setProduct(response.data);
+      setloder(false)
     } catch (error) {
       console.error('Failed to fetch Popular Products info:', error);
+      setloder(false)
     }
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
+  if (loder == true) {
+    return (
+        <div className='w-full h-screen flex items-center justify-center'>
+            <div className="w-24 h-24 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
+        </div>
+    )
+}
   return (
     <div className='w-full  p-6'>
+      <div className={popup ? `w-screen h-screen fixed left-0 z-50 top-0 flex justify-center items-center` : `hidden`}>
+        <div className="bg-blue-100 rounded text-teal-900 border border-red-500 px-4 py-3 shadow-md w-80">
+          <div>
+            <TiWarning className='text-center text-5xl text-red-500 w-full' />
+            <p className="text-lg font-medium text-center">If you delete this category, all products associated with it will also be deleted.</p>
+            <p className="font-bold text-red-500 text-center">Are you sure you want to proceed?</p>
+          </div>
+          <div className='w-full h-full flex justify-between p-3'>
+            <button className='mx-4 w-20 h-10 rounded-md bg-blue-300 text-white font-bold' onClick={() => {
+              setPopup(false);
+              setDeleteProductId(null);
+            }}> Cancel</button>
+            <button className='mx-4 w-20 h-10 rounded-md bg-red-500 text-white font-bold' onClick={deleteProduct}>Delete</button>
+          </div>
+        </div>
+      </div>
       <h1 className='font-semibold text-3xl my-2'>Products</h1>
       <div className='w-full shadow-md mt-8 border bg-white rounded-md  px-3 md:px-6 py-6'>
         <div className='w-full flex justify-between items-center'>
@@ -73,11 +106,14 @@ function Products() {
                   </td>
                   <td>
                     <div className="flex items-center my-3 justify-center">
-                      <button className="text-blue-500">
-                        <FaRegEdit className="text-xl text-blue-700" />
-                      </button>
+                      <Link to={`/admin/products/edit/${item._id}`}>
+                        <button className="text-blue-500">
+                          <FaRegEdit className="text-xl text-blue-700" />
+                        </button>
+                      </Link>
                       <button className="text-red-500 ml-2">
-                        <RiDeleteBin6Line className="text-xl text-red-500" />
+                        <RiDeleteBin6Line className="text-xl text-red-500"
+                          onClick={() => { setPopup(true); setDeleteProductId(item._id); }} />
                       </button>
                     </div>
                   </td>
