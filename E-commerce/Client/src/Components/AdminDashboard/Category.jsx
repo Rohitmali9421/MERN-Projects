c99bcabe41bddc2a4b41042d04cddef1c45b4dbb
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-31 00:21:37 +0530

    .

--- E-commerce/Client/src/Components/AdminDashboard/Category.jsx
+++ E-commerce/Client/src/Components/AdminDashboard/Category.jsx
@@ -5,9 +5,14 @@ import { IoSearch } from 'react-icons/io5';
 import axios from 'axios';
 import { useForm } from "react-hook-form";
 import { TiWarning } from "react-icons/ti";
+import { toast } from 'react-toastify';
+import { useCategory } from '../../Contexts/CategoryContext';
 
 function Category() {
-  const [image, setImage] = useState();
+  const{fetchCategory}=useCategory()
+  const [loader, setLoader] = useState(false);
+  const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState('');
   const [categories, setCategories] = useState([]);
   const { handleSubmit, register, formState: { errors }, reset } = useForm();
   const [popup, setPopup] = useState(false);
@@ -16,72 +21,93 @@ function Category() {
   const loadFile = (event) => {
     const file = event.target.files[0];
     setImage(file);
-    const output = document.getElementById('preview_img');
-    output.src = URL.createObjectURL(file);
-    output.classList.remove("hidden");
-    document.getElementById("imageupload").classList.add("hidden");
+    setImagePreview(URL.createObjectURL(file));
   };
 
   const fetchCategories = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/api/category');
-      setCategories(response.data);
+      const { data } = await axios.get('http://localhost:8000/api/category');
+      setCategories(data);
     } catch (error) {
       console.error('Failed to fetch categories:', error);
     }
   };
 
-  const addCategory = async (data) => {
+  const deleteCategory = async () => {
     try {
-      const { name } = data;
-      await axios.post('http://localhost:8000/api/category',
-        {
-          image,
-          name
-        },
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
+      setLoader(true);
+      setPopup(false);
+      const { data } = await axios.delete(`http://localhost:8000/api/category/${deleteCategoryId}`);
       fetchCategories();
-      document.getElementById('preview_img').classList.add('hidden');
-      document.getElementById('imageupload').classList.remove('hidden');
-      document.getElementById('preview_img').src = '';
-      document.getElementById('image').value = '';
-      reset();
+      setLoader(false);
+      toast.success(data.msg);
+      fetchCategory()
     } catch (error) {
-      console.error('Failed to create category:', error);
-      reset();
+      console.error('Failed to delete category:', error);
+      setLoader(false);
     }
   };
 
-  const deleteCategory = async () => {
-    //Implement it after time
+  const addCategory = async (data) => {
+    try {
+      setLoader(true);
+      const { name } = data;
+      const response=await axios.post('http://localhost:8000/api/category', { image, name }, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      });
+      fetchCategories();
+      setImage(null);
+      setImagePreview('');
+      setLoader(false);
+      toast.success(response.data.msg);
+      reset();
+      fetchCategory()
+    } catch (error) {
+      console.error('Failed to create category:', error);
+      setLoader(false);
+      reset();
+    }
   };
 
   useEffect(() => {
     fetchCategories();
   }, []);
 
+  if (loader) {
+    return (
+      <div className='w-full h-screen flex items-center justify-center'>
+        <div className="w-24 h-24 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <div className={popup ? `w-screen h-screen fixed left-0 z-50 top-0 flex justify-center items-center` : `hidden`}>
-        <div className="bg-blue-100 rounded text-teal-900 border border-red-500 px-4 py-3 shadow-md w-80">
-          <div>
-            <TiWarning className='text-center text-5xl text-red-500 w-full' />
-            <p className="text-lg font-medium text-center">If you delete this category, all products associated with it will also be deleted.</p>
-            <p className="font-bold text-red-500 text-center">Are you sure you want to proceed?</p>
-          </div>
-          <div className='w-full h-full flex justify-between p-3'>
-            <button className='mx-4 w-20 h-10 rounded-md bg-blue-300 text-white font-bold' onClick={() => {
-              setPopup(false);
-              setDeleteCategoryId(null);
-            }}> Cancel</button>
-            <button className='mx-4 w-20 h-10 rounded-md bg-red-500 text-white font-bold' onClick={deleteCategory}>Delete</button>
+      {popup && (
+        <div className='w-screen h-screen fixed left-0 z-50 top-0 flex justify-center items-center'>
+          <div className="bg-blue-100 rounded text-teal-900 border border-red-500 px-4 py-3 shadow-md w-80">
+            <div>
+              <TiWarning className='text-center text-5xl text-red-500 w-full' />
+              <p className="text-lg font-medium text-center">If you delete this category, all products associated with it will also be deleted.</p>
+              <p className="font-bold text-red-500 text-center">Are you sure you want to proceed?</p>
+            </div>
+            <div className='w-full h-full flex justify-between p-3'>
+              <button
+                className='mx-4 w-20 h-10 rounded-md bg-blue-300 text-white font-bold'
+                onClick={() => {
+                  setPopup(false);
+                  setDeleteCategoryId(null);
+                }}
+              >
+                Cancel
+              </button>
+              <button className='mx-4 w-20 h-10 rounded-md bg-red-500 text-white font-bold' onClick={deleteCategory}>
+                Delete
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className='w-full p-6'>
         <h1 className='font-semibold text-3xl my-2'>Category</h1>
         <div className='grid grid-cols-1 gap-4 lg:grid-cols-3 mt-8'>
@@ -110,11 +136,7 @@ function Category() {
                     <tr className='border-b-2' key={index}>
                       <td>
                         <div className='flex items-center my-3 w-full justify-center'>
-                          <img
-                            className='w-16 rounded-sm mr-4'
-                            src={item.imageURL.url}
-                            alt=''
-                          />
+                          <img className='w-16 rounded-sm mr-4' src={item.imageURL.url} alt={item.name} />
                         </div>
                       </td>
                       <td>
@@ -144,16 +166,14 @@ function Category() {
                   className='hidden'
                   accept='image/*'
                   onChangeCapture={loadFile}
-                  {...register("image", {
-                    required: 'Image is required',
-                  })}
+                  {...register("image", { required: 'Image is required' })}
                 />
                 <label htmlFor='image' className='cursor-pointer'>
-                  <div id='imageupload' className='w-full h-full flex flex-col justify-around items-center'>
+                  <div className={`w-full h-full flex flex-col justify-around items-center ${imagePreview ? 'hidden' : ''}`}>
                     <FaCloudUploadAlt className='text-9xl text-blue-600' />
                     <h1 className='font-normal'>Upload Category Image</h1>
                   </div>
-                  <img id='preview_img' className='mx-auto hidden' alt='Image Preview' />
+                  {imagePreview && <img id='preview_img' src={imagePreview} className='mx-auto' alt='Image Preview' />}
                 </label>
                 {errors.image && <p className="text-red-500 text-xs mt-1">{errors.image.message}</p>}
               </div>
@@ -176,10 +196,7 @@ function Category() {
                   })}
                 />
                 {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>}
-                <button
-                  type="submit"
-                  className='bg-blue-500 hover:bg-blue-700 text-white rounded mt-3 h-10 font-normal w-full'
-                >
+                <button type="submit" className='bg-blue-500 hover:bg-blue-700 text-white rounded mt-3 h-10 font-normal w-full'>
                   + Add Category
                 </button>
                 <img
