c2bff833b29523829925a1097a08306566275770
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-20 21:43:17 +0530

    .

--- E-commerce/Client/vite.config.js
+++ E-commerce/Client/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/api":"http://localhost:8000",
-      "/user":"http://localhost:8000"
+      "/api":"https://mern-server-rohit.vercel.app/",
+      "/user":"https://mern-server-rohit.vercel.app/"
     }
   },
   plugins: [react()],
