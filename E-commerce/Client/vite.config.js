0b86eaf73e5b96dee311a440943b329d8e4c8a3f
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-20 22:02:57 +0530

    .

--- E-commerce/Client/vite.config.js
+++ E-commerce/Client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   server:{
     proxy:{
       "/api":"http://localhost:8000",
