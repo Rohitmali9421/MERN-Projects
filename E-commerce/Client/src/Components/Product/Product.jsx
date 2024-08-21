252317df502f7807ecf41a1e230bb7ab7767150f
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-21 22:11:24 +0530

    .

--- E-commerce/Client/src/Components/Product/Product.jsx
+++ E-commerce/Client/src/Components/Product/Product.jsx
@@ -7,7 +7,7 @@ function Product() {
 
     const fetchProducts = async () => {
         try {
-            const response = await axios.get('/api/products');
+            const response = await axios.get('https://mern-server-rohit.vercel.app/api/products');
             setPopularProduct(response.data);
         } catch (error) {
             console.error('Failed to fetch Popular Products info:', error);
