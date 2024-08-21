4d5c1aa8abd84e8b109d0f7aefc439555ba9571b
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-21 22:15:35 +0530

    .

--- E-commerce/Server/index.js
+++ E-commerce/Server/index.js
@@ -14,10 +14,7 @@ const app = express();
 connectMongoDB(URI);
 
 //Middlerwares
-app.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific method
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
