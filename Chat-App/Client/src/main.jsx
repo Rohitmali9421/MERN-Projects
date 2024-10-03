1677322cad34b31435bacad906e296a4323e6fbc
Rohit <Rohitsmali9421@gmail.com>
2024-10-03 23:26:17 +0530

    .

--- Chat-App/Client/src/main.jsx
+++ Chat-App/Client/src/main.jsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import router from "./App.jsx"
 import { RouterProvider } from 'react-router-dom'
-
+import { Provider } from 'react-redux'
+import { store } from './App/Store.js'
 createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <RouterProvider router={router} />
+  </Provider>
 )
