0f34c56f7a137ee9cac2defe9b0b95d69848c72b
Rohit <Rohitsmali9421@gmail.com>
2025-01-24 09:28:10 +0530

    chat app

--- Chat-App/Client/src/App.jsx
+++ Chat-App/Client/src/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import Home from './Home';
-
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import { Provider } from 'react-redux';
+import { store } from "./App/Store"
+import SignUp from './Pages/SignUp';
+import Home from './Pages/Home';
+import Start from './Components/Start';
+import Chat from './Components/Chat';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<Home />} />
+
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="chat" element={<Start/>} />
+        <Route path="chat/:id" element={<Chat/>} />
+      </Route>
     </>
   )
 );
-export default router
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
+};
+
+export default App;
--- Chat-App/Client/src/main.jsx
+++ Chat-App/Client/src/main.jsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import router from "./App.jsx"
-import { RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './App/Store.js'
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-  <RouterProvider router={router} />
-  </Provider>
-)
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+);
